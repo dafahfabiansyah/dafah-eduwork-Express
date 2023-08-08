2279524cbb88070a15b1c4426dbaa25972a05c7b
dafahfabiansyah <davarobiansyah@gmail.com>
2023-08-08 10:43:58 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello world',
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello home',
+  });
+});
+
+app.get('/item', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello item',
+  });
+});
+
+app.get('/request', (req, res) => {
+  res.json({
+    path: req.path,
+    originalPath: req.originalUrl,
+    hostname: req.hostname,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port : 3000');
+});
