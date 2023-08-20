8082fbaa89b3eb16f9212586553fad7c1a8cda55
dafahfabiansyah <davarobiansyah@gmail.com>
2023-08-20 11:17:09 +0700

    update

--- api/item.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.json({
-    status: 'running',
-    message: 'hello item',
-  });
-};
--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// import express from 'express';
+import express from 'express';
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res) => {
-//   res.send({
-//     status: 'running',
-//     message: 'hello world',
-//   });
-// });
+app.get('/', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello world',
+  });
+});
 
-// app.get('/home', (req, res) => {
-//   res.send({
-//     status: 'running',
-//     message: 'hello home',
-//   });
-// });
+app.get('/home', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello home',
+  });
+});
 
-// app.get('/item', (req, res) => {
-//   res.send({
-//     status: 'running',
-//     message: 'hello item',
-//   });
-// });
+app.get('/item', (req, res) => {
+  res.send({
+    status: 'running',
+    message: 'hello item',
+  });
+});
 
-// app.get('/request', (req, res) => {
-//   res.json({
-//     path: req.path,
-//     originalPath: req.originalUrl,
-//     hostname: req.hostname,
-//   });
-// });
+app.get('/request', (req, res) => {
+  res.json({
+    path: req.path,
+    originalPath: req.originalUrl,
+    hostname: req.hostname,
+  });
+});
 
-// app.listen(3000, () => {
-//   console.log('Server is running on port : 3000');
-// });
+app.listen(3000, () => {
+  console.log('Server is running on port : 3000');
+});
