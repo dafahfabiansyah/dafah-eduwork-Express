549fe5aa1286f31655512f38d3834b077236a2ac
dafahfabiansyah <davarobiansyah@gmail.com>
2023-08-20 18:29:30 +0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const express = require('express');
-
 const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+  console.log(`API listening on PORT ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-  res.send('hello world🥳');
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h1>Hello, world 🥳</h1>
+        <a href="/about"><button>About Page</button></a>
+        <a href="/product"><button>Product Page</button></a>
+      </body>
+    </html>`;
+  res.send(htmlResponse);
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page ');
+  const aboutHtml = `
+    <html>
+      <head>
+        <title>About</title>
+      </head>
+      <body>
+        <h1>About Page</h1>
+        <a href="/"><button>Home Page</button></a>
+        <a href="/product"><button>Product Page</button></a>
+      </body>
+    </html>`;
+  res.send(aboutHtml);
+});
+
+app.get('/product', (req, res) => {
+  const productHtml = `
+    <html>
+      <head>
+        <title>Product</title>
+      </head>
+      <body>
+        <h1>Product Page</h1>
+        <a href="/"><button>Home Page</button></a>
+        <a href="/about"><button>About Page</button></a>
+      </body>
+    </html>`;
+  res.send(productHtml);
 });
 
 module.exports = app;
